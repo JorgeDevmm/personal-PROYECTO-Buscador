57abb2ede994eca47a7639e167554312a162b948
Jorge Monzon <jorge29luis10@gmail.com>
2023-03-04 13:34:56 -0500

    Leer el valor de la marca del automovil

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
 //Variables
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+// Contenedor para los resultados
+const resultado = document.querySelector('#resultado');
 
 const max = new Date().getFullYear(); //año actual
 const min = max - 10;
 
-console.log(max);
-console.log(min);
+// Generar un objeto con la búsqueda
+const datosBusqueda = {
+  marca: '',
+  year: '',
+  minimo: '',
+  maximo: '',
+  puertas: '',
+  transmision: '',
+  color: '',
+};
 
 //Eventos
 document.addEventListener('DOMContentLoaded', () => {
@@ -14,6 +30,49 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // LLena las opciones de años
   llenarSelect();
+});
+
+// Event listener para los select de búqueda
+marca.addEventListener('change', (evento) => {
+  datosBusqueda.marca = evento.target.value;
+
+  console.log(datosBusqueda);
+});
+// Event listener para los select de búqueda
+year.addEventListener('change', (evento) => {
+  datosBusqueda.year = evento.target.value;
+
+  console.log(datosBusqueda);
+});
+// Event listener para los select de búqueda
+minimo.addEventListener('change', (evento) => {
+  datosBusqueda.minimo = evento.target.value;
+
+  console.log(datosBusqueda);
+});
+// Event listener para los select de búqueda
+maximo.addEventListener('change', (evento) => {
+  datosBusqueda.maximo = evento.target.value;
+
+  console.log(datosBusqueda);
+});
+// Event listener para los select de búqueda
+puertas.addEventListener('change', (evento) => {
+  datosBusqueda.puertas = evento.target.value;
+
+  console.log(datosBusqueda);
+});
+// Event listener para los select de búqueda
+transmision.addEventListener('change', (evento) => {
+  datosBusqueda.transmision = evento.target.value;
+
+  console.log(datosBusqueda);
+});
+// Event listener para los select de búqueda
+color.addEventListener('change', (evento) => {
+  datosBusqueda.color = evento.target.value;
+
+  console.log(datosBusqueda);
 });
 
 //Funciones
