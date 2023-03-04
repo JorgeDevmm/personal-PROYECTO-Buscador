322c8e3fee3d0b82d545b23407beae2f84a406f1
Jorge Monzon <jorge29luis10@gmail.com>
2023-03-04 10:01:53 -0500

    Generar un select de años

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 //Variables
 const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
+
+const max = new Date().getFullYear(); //año actual
+const min = max - 10;
+
+console.log(max);
+console.log(min);
 
 //Eventos
 document.addEventListener('DOMContentLoaded', () => {
-  mostrarAutos();
+  mostrarAutos(); //muestra los automoviles al cargar
+
+  // LLena las opciones de años
+  llenarSelect();
 });
 
 //Funciones
@@ -21,4 +31,17 @@ function mostrarAutos() {
     // almacenamos en el div resultado los parrafos
     resultado.appendChild(autoHTML);
   });
+}
+
+// Genera los años del select
+function llenarSelect() {
+  console.log('llenando el select');
+
+  // mostrar los años de actual hacia atras
+  for (let i = max; i >= min; i--) {
+    const option = document.createElement('option');
+    option.value = i;
+    option.textContent = i;
+    year.appendChild(option); //agrega las opcion del año al select
+  }
 }
